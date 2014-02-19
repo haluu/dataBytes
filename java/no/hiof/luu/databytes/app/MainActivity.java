d8129e5adc63641d67e4958a5158000199fb8b19
Alexander Luu <alexander@luu.no>
2014-02-19 13:31:23 +0100

    add apps
    adding apps project

---
+++ java/no/hiof/luu/databytes/app/MainActivity.java
@@ -0,0 +1,17 @@
+package no.hiof.luu.databytes.app;
+
+import android.net.TrafficStats;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+        
+    }
+}
